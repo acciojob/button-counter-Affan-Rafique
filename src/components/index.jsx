882a4b02eb38444dc874e3b5167be6798f0b0034
Affan-Rafique <affanrafique9@gmail.com>
2025-11-04 15:12:39 +0530

    Create index.jsx

---
+++ src/components/index.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function ClickCounter () {
+  const [count, setCount] = useState(0);
+
+  return(
+    <div>
+       <p>Button clicked {count} times</p>
+      <button onClick = {() => setCount(count + 1)} > CLick me </button>
+    </div>
+    );
+}
+
+export default ClickCounter;
